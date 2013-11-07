4774ebf47d1fb98ce2dae7734f65679a23348fd8
Kyle <kylem3r@gmail.com>
2013-11-06 20:46:19 -0800

    Make test changes

--- RSA/src/main/java/com/pinkrose/rsa/Start.java
+++ RSA/src/main/java/com/pinkrose/rsa/Start.java
@@ -1,22 +1,19 @@
 package com.pinkrose.rsa;
 
-import android.app.Activity;
-;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+;
 
 public class Start extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -28,6 +25,7 @@ public class Start extends Activity
 
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     *
      */
     private CharSequence mTitle;
 
@@ -41,9 +39,15 @@ public class Start extends Activity
         mTitle = getTitle();
 
         // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+        mNavigationDrawerFragment.setUp
+                (
+                        R.id.navigation_drawer,
+                        (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
     }
 
     @Override
