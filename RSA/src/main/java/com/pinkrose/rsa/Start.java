83d94e14572eac8ea1c0a993a236a1ef5b7ac795
Kyle <kylem3r@gmail.com>
2013-11-06 20:55:16 -0800

    Making actual changes, up to date now with my version

--- RSA/src/main/java/com/pinkrose/rsa/Start.java
+++ RSA/src/main/java/com/pinkrose/rsa/Start.java
@@ -4,6 +4,8 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
@@ -11,7 +13,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.math.BigInteger;
+import java.util.Random;
 
 ;
 
@@ -45,9 +52,189 @@ public class Start extends Activity
                         (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
+    private EditText dataRaw, privKeyT, pubKeyT, maxKey, encDat;
+    private PrivKey privKey;
+    private PubKey pubKey;
+    private ClipboardManager cm;
+    private TextView t2, t4;
+
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+
+        ///*
+        cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        dataRaw = (EditText) findViewById(R.id.editText);
+        pubKeyT = (EditText) findViewById(R.id.editText2);
+        privKeyT = (EditText) findViewById(R.id.editText3);
+        maxKey = (EditText) findViewById(R.id.editText4);
+        encDat = (EditText) findViewById(R.id.editText5);
+
+        t2 = (TextView) findViewById(R.id.textView2);
+        t4 = (TextView) findViewById(R.id.textView4);
+
+
+        Button encB = (Button) findViewById(R.id.button);
+        Button genB = (Button) findViewById(R.id.button2);
+        Button setB = (Button) findViewById(R.id.button3);
+        Button copyB = (Button) findViewById(R.id.button4);
+        Button decB = (Button) findViewById(R.id.button5);
+        Button copyB2 = (Button) findViewById(R.id.button6);
+
+        encB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                encrypt();
+            }
+        });
+
+        setB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setKeys();
+            }
+        });
+        copyB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipData data = ClipData.newPlainText("Encrypted Text", t2.getText().toString());
+                cm.setPrimaryClip(data);
+            }
+        });
+        genB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                genKeys();
+            }
+        });
+        decB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                decrypt();
+
+            }
+        });
+        copyB2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipData data = ClipData.newPlainText("Encrypted Text", t4.getText().toString());
+                cm.setPrimaryClip(data);
+            }
+        });
+    }
+
+    public BigInteger strToNum(String s) {
+        return new BigInteger(s.getBytes());
+    }
+
+    public String numToStr(BigInteger b) {
+        String s2 = "";
+        int i;
+        while (b.compareTo(BigInteger.ONE) > 0) {
+            i = b.mod(new BigInteger("" + 256)).intValue();
+            s2 = (char) i + s2;
+            b = b.divide(new BigInteger("" + 256));
+        }
+        return s2;
+    }
+
+    public void encrypt() {
+        if (pubKey == null) genKeys();
+        if (dataRaw.getText().toString().equals("")) return;
+        BigInteger enc = strToNum(dataRaw.getText().toString());
+        BigInteger dec = enc.modPow(pubKey.getPub(), pubKey.getModu());
+        t2.setText(dec.toString());
+    }
+
+    public void decrypt() {
+        BigInteger enc = new BigInteger(encDat.getText().toString());
+        //Log.d("MEHHH",  enc.toString());
+        BigInteger raw = enc.modPow(privKey.getPriv(), pubKey.getModu());
+        t4.setText(numToStr(raw));
+    }
+
+    public void setKeys() {
+        pubKey = new PubKey(pubKeyT.getText().toString(),
+                maxKey.getText().toString());
+        privKey = new PrivKey(privKeyT.getText().toString());
+
+    }
+
+    public void genKeys() {
+        Random r = new Random();
+        Random r2 = new Random();
+        BigInteger prime1 = BigInteger.probablePrime(122, r);
+        BigInteger prime2 = BigInteger.probablePrime(126, r2);
+        BigInteger max = prime1.multiply(prime2);
+        BigInteger qui = (prime1.subtract(BigInteger.ONE)).multiply(
+                (prime2.subtract(BigInteger.ONE)));
+        BigInteger prii = new BigInteger("65537");
+        pubKey = new PubKey(new BigInteger("65537"), (max));
+        privKey = new PrivKey(prii.modInverse(qui));
+        pubKeyT.setText(pubKey.toString());
+        privKeyT.setText(privKey.toString());
+        maxKey.setText(pubKey.mToString());
+    }
+
+
+    class PrivKey {
+        private BigInteger priK;
+        private String key;
+
+        PrivKey(String priv) {
+            key = priv;
+            priK = strToNum(priv);
+        }
+
+        PrivKey(BigInteger priv) {
+            key = numToStr(priv);
+            priK = (priv);
+        }
+
+        public BigInteger getPriv() {
+            return priK;
+        }
+
+        public String toString() {
+            return key;
+        }
+    }
+
+    class PubKey {
+        private BigInteger pub;
+        private BigInteger modu;
+        private String mod;
+
+        PubKey(BigInteger exp, BigInteger mod) {
+            //key = numToStr(exp);
+            this.mod = numToStr(mod);
+            pub = exp;
+            modu = (mod);
+        }
+
+        PubKey(String exp, String mod) {
+            //key = exp;
+            this.mod = (mod);
+            pub = new BigInteger(exp);
+            modu = strToNum(mod);
+        }
+
+        public BigInteger getModu() {
+            return modu;
+        }
+
+        public BigInteger getPub() {
+            return pub;
+        }
+
+        public String toString() {
+            return pub.toString();
+        }
+
+        public String mToString() {
+            return mod;
+        }
     }
 
     @Override
